fa461384939d71da8d3f0e67690405d5634762d6
Teterschaft <gofla2242@tukorea.ac.kr>
2025-09-05 21:45:33 +0900

    feat: 기본 타입 정의

---
+++ CoreLib/Types/Types.h
@@ -0,0 +1,14 @@
+﻿#pragma once
+
+// 기본 타입 정의
+typedef signed char         int8;
+typedef short               int16;
+typedef int                 int32;
+typedef long long           int64;
+
+typedef unsigned char       uint8;
+typedef unsigned short      uint16;
+typedef unsigned int        uint32;
+typedef unsigned long long  uint64;
+
+using int8 = signed char;
