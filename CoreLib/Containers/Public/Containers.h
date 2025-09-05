79036166a5d7f9722eae1b48ce3ed84412964060
Teterschaft <gofla2242@tukorea.ac.kr>
2025-09-05 21:40:37 +0900

    feat: STL 컨테이너 타입 재정의

---
+++ CoreLib/Containers/Public/Containers.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <vector>
+#include <list>
+#include <unordered_map>
+#include <unordered_set>
+#include <array>
+#include <queue>
+
+template <typename T> using TArray = std::vector<T>;
+template <typename T> using TLinkedList = std::list<T>;
+template <typename T> using TSet = std::unordered_set<T>;
+template <typename K, typename V> using TMap = std::unordered_map<K, V>;
+template <typename K, typename V> using TPair = std::pair<K, V>;
+template <typename T, unsigned N> using TStaticArray = std::array<T, N>;
+template <typename T> using TQueue = std::queue<T>;
+
+using FString = std::string;
