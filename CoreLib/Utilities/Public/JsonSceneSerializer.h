8b7ac9740c138d3a8c3780771ee4586c6cb1f42e
Teterschaft <gofla2242@tukorea.ac.kr>
2025-09-07 00:27:48 +0900

    feat: Scene Serializer 작업

---
+++ CoreLib/Utilities/Public/JsonSceneSerializer.h
@@ -0,0 +1,110 @@
+#pragma once
+#include "Math/Math.h"
+#include "Containers/Containers.h"
+#include "Json/json.hpp"
+#include "Object/Object.h"
+#include "Actor/Actor.h"
+#include "Component/Component.h"
+#include "Renderer/Renderer.h"
+#include <fstream>
+
+// TODO: 임시 Primivite Type, 나중에 만들어줘야함
+enum class EPrimitiveType
+{
+	Sphere,
+	Cube,
+	Triangle
+};
+
+FString PrimitiveTypeToString(EPrimitiveType Type)
+{
+	switch (Type)
+	{
+	case EPrimitiveType::Sphere:
+		return "Sphere";
+	case EPrimitiveType::Cube:
+		return "Cube";
+	case EPrimitiveType::Triangle:
+		return "Triangle";
+	default:
+		return "Unknown";
+	}
+}
+
+void SavePrimitive(json::JSON& Obj, int Index, FVector Location, FVector Rotation, FVector Scale, EPrimitiveType Type)
+{
+	FString IndexStr = std::to_string(Index);
+	Obj["Primitives"][IndexStr] = json::Object();
+	Obj["Primitives"][IndexStr]["Location"] = json::Array(Location.X, Location.Y, Location.Z);
+	Obj["Primitives"][IndexStr]["Rotation"] = json::Array(Rotation.X, Rotation.Y, Rotation.Z);
+	Obj["Primitives"][IndexStr]["Scale"] = json::Array(Scale.X, Scale.Y, Scale.Z);
+	Obj["Primitives"][IndexStr]["Type"] = PrimitiveTypeToString(Type);
+}
+
+void SaveScene(const FString& FilePath, int32 Version)
+{
+	json::JSON Obj = json::Object();
+	Obj["Version"] = Version;
+	Obj["NextUUID"] = GUDObjectArray.size() + 1;
+	Obj["Primitives"] = json::Object();
+	for (size_t i = 0; i < GUDObjectArray.size(); ++i)
+	{
+		auto Actor = static_cast<AActor*>(GUDObjectArray[i]);
+		if (Actor)
+		{
+			auto PrimitiveComponent = Actor->GetComponent<UPrimitiveComponent>();
+			if (PrimitiveComponent)
+			{
+				FVector Location = FVector(i, i, i);
+				FVector Rotation = FVector(i, i, i);
+				FVector Scale = FVector(i, i, i);
+				EPrimitiveType Type = EPrimitiveType::Triangle; // Default to Triangle
+				SavePrimitive(Obj, static_cast<int>(i), Location, Rotation, Scale, Type);
+			}
+		}
+	}
+
+	std::ofstream file(FilePath.c_str());
+	file << Obj.dump(1);
+}
+
+// TODO: 미완성 함수
+void LoadScene(const FString& FilePath)
+{
+	// 처음에 밀어버리는 기능도 있어야함
+	std::ifstream file(FilePath.c_str());
+	if (!file.is_open())
+	{
+		return;
+	}
+	std::string content((std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>());
+	json::JSON Obj = json::JSON::Load(content);
+	int32 NextUUID = Obj["NextUUID"].ToInt();
+	if (NextUUID > GUDObjectArray.size() + 1)
+	{
+		GUDObjectArray.resize(NextUUID - 1, nullptr);
+	}
+
+	json::JSON Primitives = Obj["Primitives"];
+	for (const auto& p : Primitives.ObjectRange())
+	{
+		int Index = std::stoi(p.first);
+		json::JSON Primitive = p.second;
+
+		FVector Location = FVector(
+			Primitive["Location"][0].ToFloat(),
+			Primitive["Location"][1].ToFloat(),
+			Primitive["Location"][2].ToFloat()
+		);
+		FVector Rotation = FVector(
+			Primitive["Rotation"][0].ToFloat(),
+			Primitive["Rotation"][1].ToFloat(),
+			Primitive["Rotation"][2].ToFloat()
+		);
+		FVector Scale = FVector(
+			Primitive["Scale"][0].ToFloat(),
+			Primitive["Scale"][1].ToFloat(),
+			Primitive["Scale"][2].ToFloat()
+		);
+	}
+}
