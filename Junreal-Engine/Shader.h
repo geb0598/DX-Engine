e05fcafd7e756f7fc129901129ac2fbf6c175505
geb0598 <geb0598@gmail.com>
2025-10-19 16:47:44 +0900

    comment: 사용법 주석 추가

--- Junreal-Engine/Shader.h
+++ Junreal-Engine/Shader.h
@@ -6,7 +6,17 @@ class UShader : public UResourceBase
 public:
 	DECLARE_CLASS(UShader, UResourceBase)
 
-	void Load(const FString& ShaderPath, ID3D11Device* InDevice);
+	/**
+	 * @brief 커맨드라인 형식의 문자열을 파싱하여 셰이더를 로드하고 컴파일합니다.
+	 * @param InCommandString 셰이더 파일 경로, 매크로, 진입점 등을 포함하는 명령어 문자열.
+	 * 포맷: "filepath [-D MACRO=VALUE] [-VS vs_entry] [-PS ps_entry]"
+	 * - **filepath**: 컴파일할 .hlsl 파일의 경로.
+	 * - **-D [MACRO_NAME](=[VALUE])**: 전처리기 매크로를 정의합니다. VALUE가 없으면 1로 정의됩니다. (예: -D USE_TEXTURE=1)
+	 * - **-VS [entry_point]**: Vertex Shader의 진입점 함수 이름을 지정합니다. (기본값: mainVS)
+	 * - **-PS [entry_point]**: Pixel Shader의 진입점 함수 이름을 지정합니다. (기본값: mainPS)
+	 * @param InDevice 셰이더 객체를 생성할 D3D11 디바이스.
+	 */
+	void Load(const FString& InCommandString, ID3D11Device* InDevice);
 
 	ID3D11InputLayout* GetInputLayout() const { return InputLayout; }
 	ID3D11VertexShader* GetVertexShader() const { return VertexShader; }
