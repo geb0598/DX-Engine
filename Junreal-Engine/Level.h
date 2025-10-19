aabff2c1f29a894baf314bf2cb237f74cd744abb
sb-111 <117798778+sb-111@users.noreply.github.com>
2025-10-20 01:16:48 +0900

    feat: Level이 LightComponent를 수집 및 반환할 수 있는 기능 추가

--- Junreal-Engine/Level.h
+++ Junreal-Engine/Level.h
@@ -9,6 +9,11 @@ class UDecalComponent;
 class UPrimitiveComponent;
 class UFireBallComponent;
 class UFXAAComponent;
+class UAmbientLightComponent;
+class UDirectionalLightComponent;
+class UPointLightComponent;
+class USpotLightComponent;
+
 
 class ULevel : public UObject
 {
@@ -24,6 +29,8 @@ public:
 	template<typename T>
 	TArray<T*>& GetComponentList();
 	
+	template<typename T>
+	T* GetComponent();
 
 	const TArray<AActor*>& GetActors() const;
 	TArray<AActor*>& GetActors();
@@ -35,6 +42,10 @@ private:
 	TArray<UPrimitiveComponent*> PrimitiveComponentList;
 	TArray<UFireBallComponent*> FireBallComponentList;
 	TArray<UFXAAComponent*> FXAAComponentList;
+	UAmbientLightComponent* AmbientLightComponent;
+	UDirectionalLightComponent* DirectionalLightComponent;
+	TArray<UPointLightComponent*> PointLightComponentList;
+	TArray<USpotLightComponent*> SpotLightComponentList;
 };
 
 template<> TArray<UExponentialHeightFogComponent*>& ULevel::GetComponentList<UExponentialHeightFogComponent>();
@@ -43,3 +54,8 @@ template<> TArray<UDecalComponent*>& ULevel::GetComponentList<UDecalComponent>()
 template<> TArray<UPrimitiveComponent*>& ULevel::GetComponentList<UPrimitiveComponent>();
 template<> TArray<UFireBallComponent*>& ULevel::GetComponentList<UFireBallComponent>();
 template<> TArray<UFXAAComponent*>& ULevel::GetComponentList<UFXAAComponent>();
+template<> TArray<UPointLightComponent*>& ULevel::GetComponentList<UPointLightComponent>();
+template<> TArray<USpotLightComponent*>& ULevel::GetComponentList<USpotLightComponent>();
+
+template<> UAmbientLightComponent* ULevel::GetComponent<UAmbientLightComponent>();
+template<> UDirectionalLightComponent* ULevel::GetComponent<UDirectionalLightComponent>();
