84e9ab16a014932252d5977ef5af6043758b01d2
Donghee <luz0415@naver.com>
2025-11-03 22:06:49 +0900

    feat: Player Light Exposure UI 추가

--- Engine/Source/Demo/Private/Player.cpp
+++ Engine/Source/Demo/Private/Player.cpp
@@ -2,6 +2,7 @@
 #include "Demo/Public/Player.h"
 
 #include "Component/Public/BoxComponent.h"
+#include "Component/Public/LightSensorComponent.h"
 #include "Component/Public/ScriptComponent.h"
 
 IMPLEMENT_CLASS(APlayer, AActor)
@@ -21,5 +22,8 @@ UClass* APlayer::GetDefaultRootComponent()
 void APlayer::InitializeComponents()
 {
 	Super::InitializeComponents();
-	Cast<UBoxComponent>(GetRootComponent())->SetBoxExtent({1.0f, 1.0f, 1.0f});
+	Cast<UBoxComponent>(GetRootComponent())->SetBoxExtent({0.5f, 0.5f, 0.5f});
+
+	ULightSensorComponent* LightSensor = CreateDefaultSubobject<ULightSensorComponent>();
+	LightSensor->AttachToComponent(GetRootComponent());
 }
