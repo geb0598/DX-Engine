704c281e73019850a0fe7582718acc51ff84e8cd
TraceofLight <rickyjun96@gmail.com>
2025-11-04 15:01:03 +0900

    feat: use dgpu
    고성능 외장 그래픽을 사용하도록 힌트를 주는 코드 삽입

--- Engine/main.cpp
+++ Engine/main.cpp
@@ -1,6 +1,14 @@
 #include "pch.h"
 #include "Core/Public/ClientApp.h"
 
+extern "C" {
+	__declspec(dllexport) DWORD NvOptimusEnablement = 0x00000001;
+}
+
+extern "C" {
+	__declspec(dllexport) int AmdPowerXpressRequestHighPerformance = 1;
+}
+
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd)
 {
     UNREFERENCED_PARAMETER(hPrevInstance);
