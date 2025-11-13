5dfd4645978f5f7c8e2cdfae12de4c7eacc5cb2e
Donghee <luz0415@naver.com>
2025-11-13 16:22:06 +0900

    init repo

---
+++ Mundi/Source/Runtime/AssetManagement/SkeletalMesh.cpp
@@ -0,0 +1,79 @@
+﻿#include "pch.h"
+#include "SkeletalMesh.h"
+
+
+#include "FbxLoader.h"
+#include "WindowsBinReader.h"
+#include "WindowsBinWriter.h"
+#include "PathUtils.h"
+#include <filesystem>
+
+IMPLEMENT_CLASS(USkeletalMesh)
+
+USkeletalMesh::USkeletalMesh()
+{
+}
+
+USkeletalMesh::~USkeletalMesh()
+{
+    ReleaseResources();
+}
+
+void USkeletalMesh::Load(const FString& InFilePath, ID3D11Device* InDevice)
+{
+    if (Data)
+    {
+        ReleaseResources();
+    }
+
+    // FBXLoader가 캐싱을 내부적으로 처리합니다
+    Data = UFbxLoader::GetInstance().LoadFbxMeshAsset(InFilePath);
+
+    if (!Data || Data->Vertices.empty() || Data->Indices.empty())
+    {
+        UE_LOG("ERROR: Failed to load FBX mesh from '%s'", InFilePath.c_str());
+        return;
+    }
+
+    // GPU 버퍼 생성
+    CreateIndexBuffer(Data, InDevice);
+    VertexCount = static_cast<uint32>(Data->Vertices.size());
+    IndexCount = static_cast<uint32>(Data->Indices.size());
+    VertexStride = sizeof(FVertexDynamic);
+}
+
+void USkeletalMesh::ReleaseResources()
+{
+    if (IndexBuffer)
+    {
+        IndexBuffer->Release();
+        IndexBuffer = nullptr;
+    }
+
+    if (Data)
+    {
+        delete Data;
+        Data = nullptr;
+    }
+}
+
+void USkeletalMesh::CreateVertexBuffer(ID3D11Buffer** InVertexBuffer)
+{
+    if (!Data) { return; }
+    ID3D11Device* Device = GEngine.GetRHIDevice()->GetDevice();
+    HRESULT hr = D3D11RHI::CreateVertexBuffer<FVertexDynamic>(Device, Data->Vertices, InVertexBuffer);
+    assert(SUCCEEDED(hr));
+}
+
+void USkeletalMesh::UpdateVertexBuffer(const TArray<FNormalVertex>& SkinnedVertices, ID3D11Buffer* InVertexBuffer)
+{
+    if (!InVertexBuffer) { return; }
+
+    GEngine.GetRHIDevice()->VertexBufferUpdate(InVertexBuffer, SkinnedVertices);
+}
+
+void USkeletalMesh::CreateIndexBuffer(FSkeletalMeshData* InSkeletalMesh, ID3D11Device* InDevice)
+{
+    HRESULT hr = D3D11RHI::CreateIndexBuffer(InDevice, InSkeletalMesh, &IndexBuffer);
+    assert(SUCCEEDED(hr));
+}
