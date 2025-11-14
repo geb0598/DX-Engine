2eaa0dbd5ad167b26778d98415bc28566697a995
GameTith <wkdwobum@gmail.com>
2025-11-14 15:53:51 +0900

    feat: 현재 애니메이션의 상태를 얻어오는 함수 추가

---
+++ Mundi/Source/Runtime/Engine/Animation/AnimationRuntime.h
@@ -0,0 +1,8 @@
+﻿#pragma once
+#include "AnimSequenceBase.h"
+
+class FAnimationRuntime
+{
+	static ETypeAdvanceAnim AdvanceTime(const bool bAllowLooping, const float MoveDelta, float& InOutTime, const float EndTime);
+
+};
