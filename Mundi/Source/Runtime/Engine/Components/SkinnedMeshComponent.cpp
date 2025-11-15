85fef6bf1a17531a9afb497e39161c7e1027391d
WH <121390020+Jeongwoohyeong@users.noreply.github.com>
2025-11-15 20:31:22 +0900

    misc : cpu스키닝 프로파일링 주석 해제

--- Mundi/Source/Runtime/Engine/Components/SkinnedMeshComponent.cpp
+++ Mundi/Source/Runtime/Engine/Components/SkinnedMeshComponent.cpp
@@ -347,7 +347,7 @@ void USkinnedMeshComponent::PerformSkinning()
       return;
    }
 
-   //TIME_PROFILE(CPUSkinning)
+   TIME_PROFILE(CPUSkinning)
    const TArray<FSkinnedVertex>& SrcVertices = SkeletalMesh->GetSkeletalMeshData()->Vertices;
    const int32 NumVertices = SrcVertices.Num();
    SkinnedVertices.SetNum(NumVertices);
@@ -362,7 +362,7 @@ void USkinnedMeshComponent::PerformSkinning()
       DstVert.Tangent = SkinVertexTangent(SrcVert);
       DstVert.tex = SrcVert.UV;
    }
-   //TIME_PROFILE_END(CPUSkinning)   
+   TIME_PROFILE_END(CPUSkinning)   
 }
 
 void USkinnedMeshComponent::UpdateSkinningMatrices(const TArray<FMatrix>& InSkinningMatrices, const TArray<FMatrix>& InSkinningNormalMatrices)
