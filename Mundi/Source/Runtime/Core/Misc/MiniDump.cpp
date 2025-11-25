d372120774d11b05f3408b2510d6feeada2cd6fb
changgeun <hoi000115@gmail.com>
2025-11-26 01:26:57 +0900

    feat: 깃에서 소스파일 다운받아서 호출스택 확인할 수 있도록 수정

--- Mundi/Source/Runtime/Core/Misc/MiniDump.cpp
+++ Mundi/Source/Runtime/Core/Misc/MiniDump.cpp
@@ -6,6 +6,157 @@
 #include <iomanip>
 #include <random>
 #include <new.h> // _set_new_handler 용
+#include <fstream>
+
+#pragma comment(lib, "dbghelp.lib")
+
+// 빌드 시점의 Git 정보 (PreBuildEvent에서 생성됨)
+#if __has_include("GitVersion.h")
+#include "GitVersion.h"
+#endif
+
+#ifndef GIT_COMMIT_HASH
+#define GIT_COMMIT_HASH "unknown"
+#endif
+
+#ifndef GIT_REPO_URL
+#define GIT_REPO_URL "unknown"
+#endif
+
+// ============================================================================
+// 호출 스택을 캡처하여 문자열로 반환
+// ============================================================================
+std::wstring CaptureCallStack(CONTEXT* Context)
+{
+    std::wstringstream StackTrace;
+
+    HANDLE hProcess = GetCurrentProcess();
+    HANDLE hThread = GetCurrentThread();
+
+    // 심볼 초기화
+    SymSetOptions(SYMOPT_UNDNAME | SYMOPT_DEFERRED_LOADS | SYMOPT_LOAD_LINES);
+    if (!SymInitialize(hProcess, nullptr, TRUE))
+    {
+        StackTrace << L"Failed to initialize symbols. Error: " << GetLastError() << L"\r\n";
+        return StackTrace.str();
+    }
+
+    // 스택 프레임 설정
+    STACKFRAME64 StackFrame = {};
+    DWORD MachineType;
+
+#ifdef _M_X64
+    MachineType = IMAGE_FILE_MACHINE_AMD64;
+    StackFrame.AddrPC.Offset = Context->Rip;
+    StackFrame.AddrPC.Mode = AddrModeFlat;
+    StackFrame.AddrFrame.Offset = Context->Rbp;
+    StackFrame.AddrFrame.Mode = AddrModeFlat;
+    StackFrame.AddrStack.Offset = Context->Rsp;
+    StackFrame.AddrStack.Mode = AddrModeFlat;
+#else
+    MachineType = IMAGE_FILE_MACHINE_I386;
+    StackFrame.AddrPC.Offset = Context->Eip;
+    StackFrame.AddrPC.Mode = AddrModeFlat;
+    StackFrame.AddrFrame.Offset = Context->Ebp;
+    StackFrame.AddrFrame.Mode = AddrModeFlat;
+    StackFrame.AddrStack.Offset = Context->Esp;
+    StackFrame.AddrStack.Mode = AddrModeFlat;
+#endif
+
+    StackTrace << L"========== Call Stack ==========\r\n";
+
+    // 심볼 정보를 위한 버퍼
+    char SymbolBuffer[sizeof(SYMBOL_INFO) + MAX_SYM_NAME * sizeof(TCHAR)];
+    PSYMBOL_INFO Symbol = (PSYMBOL_INFO)SymbolBuffer;
+    Symbol->SizeOfStruct = sizeof(SYMBOL_INFO);
+    Symbol->MaxNameLen = MAX_SYM_NAME;
+
+    IMAGEHLP_LINE64 Line = {};
+    Line.SizeOfStruct = sizeof(IMAGEHLP_LINE64);
+
+    int FrameIndex = 0;
+    const int MaxFrames = 64;
+
+    while (FrameIndex < MaxFrames)
+    {
+        if (!StackWalk64(MachineType, hProcess, hThread, &StackFrame, Context,
+            nullptr, SymFunctionTableAccess64, SymGetModuleBase64, nullptr))
+        {
+            break;
+        }
+
+        if (StackFrame.AddrPC.Offset == 0)
+        {
+            break;
+        }
+
+        DWORD64 Address = StackFrame.AddrPC.Offset;
+        DWORD64 Displacement64 = 0;
+        DWORD Displacement = 0;
+
+        StackTrace << L"[" << FrameIndex << L"] 0x" << std::hex << Address << std::dec;
+
+        // 함수명 가져오기
+        if (SymFromAddr(hProcess, Address, &Displacement64, Symbol))
+        {
+            // char를 wchar_t로 변환
+            wchar_t WideName[MAX_SYM_NAME];
+            size_t Converted;
+            mbstowcs_s(&Converted, WideName, Symbol->Name, MAX_SYM_NAME);
+            StackTrace << L" " << WideName;
+        }
+
+        // 파일명과 라인 번호 가져오기
+        if (SymGetLineFromAddr64(hProcess, Address, &Displacement, &Line))
+        {
+            wchar_t WideFileName[MAX_PATH];
+            size_t Converted;
+            mbstowcs_s(&Converted, WideFileName, Line.FileName, MAX_PATH);
+            StackTrace << L" (" << WideFileName << L":" << Line.LineNumber << L")";
+        }
+
+        StackTrace << L"\r\n";
+        FrameIndex++;
+    }
+
+    StackTrace << L"================================\r\n";
+
+    SymCleanup(hProcess);
+
+    return StackTrace.str();
+}
+
+// ============================================================================
+// 예외 정보를 문자열로 변환
+// ============================================================================
+std::wstring GetExceptionDescription(DWORD ExceptionCode)
+{
+    switch (ExceptionCode)
+    {
+    case EXCEPTION_ACCESS_VIOLATION:         return L"Access Violation";
+    case EXCEPTION_ARRAY_BOUNDS_EXCEEDED:    return L"Array Bounds Exceeded";
+    case EXCEPTION_BREAKPOINT:               return L"Breakpoint";
+    case EXCEPTION_DATATYPE_MISALIGNMENT:    return L"Datatype Misalignment";
+    case EXCEPTION_FLT_DENORMAL_OPERAND:     return L"Float Denormal Operand";
+    case EXCEPTION_FLT_DIVIDE_BY_ZERO:       return L"Float Divide By Zero";
+    case EXCEPTION_FLT_INEXACT_RESULT:       return L"Float Inexact Result";
+    case EXCEPTION_FLT_INVALID_OPERATION:    return L"Float Invalid Operation";
+    case EXCEPTION_FLT_OVERFLOW:             return L"Float Overflow";
+    case EXCEPTION_FLT_STACK_CHECK:          return L"Float Stack Check";
+    case EXCEPTION_FLT_UNDERFLOW:            return L"Float Underflow";
+    case EXCEPTION_ILLEGAL_INSTRUCTION:      return L"Illegal Instruction";
+    case EXCEPTION_IN_PAGE_ERROR:            return L"In Page Error";
+    case EXCEPTION_INT_DIVIDE_BY_ZERO:       return L"Integer Divide By Zero";
+    case EXCEPTION_INT_OVERFLOW:             return L"Integer Overflow";
+    case EXCEPTION_INVALID_DISPOSITION:      return L"Invalid Disposition";
+    case EXCEPTION_NONCONTINUABLE_EXCEPTION: return L"Noncontinuable Exception";
+    case EXCEPTION_PRIV_INSTRUCTION:         return L"Privileged Instruction";
+    case EXCEPTION_SINGLE_STEP:              return L"Single Step";
+    case EXCEPTION_STACK_OVERFLOW:           return L"Stack Overflow";
+    case 0xC0000374:                         return L"Heap Corruption";
+    default:                                 return L"Unknown Exception";
+    }
+}
 
 // ============================================================================
 // 1. 덤프 생성 로직을 함수 하나로 분리 (재사용 위해)
@@ -95,6 +246,150 @@ void CreateMiniDump(struct _EXCEPTION_POINTERS* ExceptionInfo)
                 std::wstring ExeSourcePath = ExeDir + L"Mundi.exe";
                 std::wstring ExeDestPath = CrashFolderPath + L"Mundi.exe";
                 CopyFileW(ExeSourcePath.c_str(), ExeDestPath.c_str(), FALSE);
+
+                // 호출 스택 정보를 텍스트 파일로 저장
+                std::wstring StackTraceDestPath = CrashFolderPath + L"CallStack.txt";
+                HANDLE hStackFile = CreateFileW(StackTraceDestPath.c_str(),
+                    GENERIC_WRITE, 0, nullptr,
+                    CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, nullptr);
+
+                if (hStackFile != INVALID_HANDLE_VALUE)
+                {
+                    std::wstringstream CrashReport;
+
+                    // 크래시 시간 정보
+                    CrashReport << L"========== Crash Report ==========\r\n";
+                    CrashReport << L"Time: " << std::put_time(&TimeInfo, L"%Y-%m-%d %H:%M:%S") << L"\r\n";
+
+                    // Git 정보
+                    CrashReport << L"Git Commit: " << GIT_COMMIT_HASH << L"\r\n";
+                    CrashReport << L"Git Repo: " << GIT_REPO_URL << L"\r\n";
+
+                    // 예외 정보
+                    if (ExceptionInfo && ExceptionInfo->ExceptionRecord)
+                    {
+                        DWORD ExCode = ExceptionInfo->ExceptionRecord->ExceptionCode;
+                        CrashReport << L"Exception Code: 0x" << std::hex << ExCode << std::dec << L"\r\n";
+                        CrashReport << L"Exception Type: " << GetExceptionDescription(ExCode) << L"\r\n";
+                        CrashReport << L"Exception Address: 0x" << std::hex
+                                    << (DWORD64)ExceptionInfo->ExceptionRecord->ExceptionAddress << std::dec << L"\r\n";
+
+                        // Access Violation인 경우 추가 정보
+                        if (ExCode == EXCEPTION_ACCESS_VIOLATION && ExceptionInfo->ExceptionRecord->NumberParameters >= 2)
+                        {
+                            ULONG_PTR AccessType = ExceptionInfo->ExceptionRecord->ExceptionInformation[0];
+                            ULONG_PTR AccessAddress = ExceptionInfo->ExceptionRecord->ExceptionInformation[1];
+                            CrashReport << L"Access Type: " << (AccessType == 0 ? L"Read" : (AccessType == 1 ? L"Write" : L"Execute")) << L"\r\n";
+                            CrashReport << L"Access Address: 0x" << std::hex << AccessAddress << std::dec << L"\r\n";
+                        }
+                    }
+                    else
+                    {
+                        CrashReport << L"Exception Info: Not available (CRT handler)\r\n";
+                    }
+
+                    CrashReport << L"\r\n";
+
+                    // 호출 스택 캡처
+                    if (ExceptionInfo && ExceptionInfo->ContextRecord)
+                    {
+                        CrashReport << CaptureCallStack(ExceptionInfo->ContextRecord);
+                    }
+                    else
+                    {
+                        // ExceptionInfo가 없는 경우 현재 컨텍스트 캡처
+                        CONTEXT CurrentContext = {};
+                        CurrentContext.ContextFlags = CONTEXT_FULL;
+                        RtlCaptureContext(&CurrentContext);
+                        CrashReport << CaptureCallStack(&CurrentContext);
+                    }
+
+                    // 파일에 쓰기 (UTF-8 BOM + UTF-8로 변환)
+                    std::wstring Report = CrashReport.str();
+
+                    // UTF-8 BOM 쓰기
+                    unsigned char BOM[] = { 0xEF, 0xBB, 0xBF };
+                    DWORD BytesWritten;
+                    WriteFile(hStackFile, BOM, sizeof(BOM), &BytesWritten, nullptr);
+
+                    // wstring을 UTF-8로 변환하여 쓰기
+                    int Utf8Size = WideCharToMultiByte(CP_UTF8, 0, Report.c_str(), -1, nullptr, 0, nullptr, nullptr);
+                    if (Utf8Size > 0)
+                    {
+                        char* Utf8Buffer = new char[Utf8Size];
+                        WideCharToMultiByte(CP_UTF8, 0, Report.c_str(), -1, Utf8Buffer, Utf8Size, nullptr, nullptr);
+                        WriteFile(hStackFile, Utf8Buffer, Utf8Size - 1, &BytesWritten, nullptr); // -1 to exclude null terminator
+                        delete[] Utf8Buffer;
+                    }
+
+                    CloseHandle(hStackFile);
+                }
+
+                // OpenDump.bat 생성 - 실행하면 자동으로 git clone 받고 덤프 열기
+                std::wstring BatFilePath = CrashFolderPath + L"OpenDump.bat";
+                HANDLE hBatFile = CreateFileW(BatFilePath.c_str(),
+                    GENERIC_WRITE, 0, nullptr,
+                    CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, nullptr);
+
+                if (hBatFile != INVALID_HANDLE_VALUE)
+                {
+                    std::stringstream BatContent;
+                    BatContent << "@echo off\r\n";
+                    BatContent << "chcp 65001 > nul\r\n";
+                    BatContent << "setlocal\r\n";
+                    BatContent << "\r\n";
+                    BatContent << "set \"CRASH_DIR=%~dp0\"\r\n";
+                    BatContent << "set \"SOURCE_DIR=%CRASH_DIR%Source\"\r\n";
+                    BatContent << "set \"GIT_REPO=" << GIT_REPO_URL << "\"\r\n";
+                    BatContent << "set \"GIT_COMMIT=" << GIT_COMMIT_HASH << "\"\r\n";
+                    BatContent << "\r\n";
+                    BatContent << "echo ========================================\r\n";
+                    BatContent << "echo   Mundi Crash Dump Analyzer\r\n";
+                    BatContent << "echo ========================================\r\n";
+                    BatContent << "echo.\r\n";
+                    BatContent << "echo Git Repo: %GIT_REPO%\r\n";
+                    BatContent << "echo Git Commit: %GIT_COMMIT%\r\n";
+                    BatContent << "echo.\r\n";
+                    BatContent << "\r\n";
+                    BatContent << "if exist \"%SOURCE_DIR%\" (\r\n";
+                    BatContent << "    echo Source folder already exists. Skipping clone.\r\n";
+                    BatContent << ") else (\r\n";
+                    BatContent << "    echo Cloning repository...\r\n";
+                    BatContent << "    git clone \"%GIT_REPO%\" \"%SOURCE_DIR%\"\r\n";
+                    BatContent << "    if errorlevel 1 (\r\n";
+                    BatContent << "        echo Failed to clone repository!\r\n";
+                    BatContent << "        pause\r\n";
+                    BatContent << "        exit /b 1\r\n";
+                    BatContent << "    )\r\n";
+                    BatContent << ")\r\n";
+                    BatContent << "\r\n";
+                    BatContent << "echo Checking out commit %GIT_COMMIT%...\r\n";
+                    BatContent << "cd /d \"%SOURCE_DIR%\"\r\n";
+                    BatContent << "git checkout %GIT_COMMIT%\r\n";
+                    BatContent << "if errorlevel 1 (\r\n";
+                    BatContent << "    echo Failed to checkout commit!\r\n";
+                    BatContent << "    pause\r\n";
+                    BatContent << "    exit /b 1\r\n";
+                    BatContent << ")\r\n";
+                    BatContent << "\r\n";
+                    BatContent << "echo.\r\n";
+                    BatContent << "echo Source ready! Opening dump file...\r\n";
+                    BatContent << "echo.\r\n";
+                    BatContent << "echo [TIP] In Visual Studio:\r\n";
+                    BatContent << "echo   1. Click 'Debug with Native Only'\r\n";
+                    BatContent << "echo   2. If source not found, browse to:\r\n";
+                    BatContent << "echo      %SOURCE_DIR%\\Mundi\\Source\r\n";
+                    BatContent << "echo.\r\n";
+                    BatContent << "\r\n";
+                    BatContent << "start \"\" \"%CRASH_DIR%Mundi_CrashDump.dmp\"\r\n";
+                    BatContent << "\r\n";
+                    BatContent << "pause\r\n";
+
+                    std::string BatStr = BatContent.str();
+                    DWORD BytesWritten;
+                    WriteFile(hBatFile, BatStr.c_str(), (DWORD)BatStr.size(), &BytesWritten, nullptr);
+                    CloseHandle(hBatFile);
+                }
             }
         }
         // 서버 접속 실패 시 로컬 덤프만 남기고 계속 진행
