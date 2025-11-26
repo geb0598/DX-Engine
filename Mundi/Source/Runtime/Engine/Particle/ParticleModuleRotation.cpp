0f39917e573839059d89ae89eff65442a30cbfef
geb0598 <geb0598@gmail.com>
2025-11-26 15:22:44 +0900

    feat: 파티클 회전 모듈

---
+++ Mundi/Source/Runtime/Engine/Particle/ParticleModuleRotation.cpp
@@ -0,0 +1,32 @@
+﻿#include "pch.h"
+#include "ParticleModuleRotation.h"
+
+#include "ParticleHelper.h"
+
+UParticleModuleRotation::UParticleModuleRotation()
+{
+	bSpawnModule = true;
+
+	InitializeDefaults();
+}
+
+void UParticleModuleRotation::InitializeDefaults()
+{
+	if (!StartRotation.IsCreated())
+	{
+		UDistributionFloatUniform* DistributionStartRotation = NewObject<UDistributionFloatUniform>();
+		DistributionStartRotation->Min = 0.0f;
+		DistributionStartRotation->Max = 1.0f;
+		StartRotation.Distribution = DistributionStartRotation;
+	}
+}
+
+void UParticleModuleRotation::Spawn(const FSpawnContext& Context)
+{
+	FParticleEmitterInstance* Owner = &Context.Owner;
+
+	SPAWN_INIT
+	{
+		Particle.Rotation += (PI/180.0f) * 360.0f * StartRotation.GetValue(Owner->EmitterTime);
+	}
+}
---
+++ Mundi/Source/Runtime/Engine/Particle/ParticleModuleRotation.h
@@ -0,0 +1,28 @@
+﻿#pragma once
+#include "ParticleModule.h"
+#include "Source/Runtime/Engine/Distribution/Distributions.h"
+#include "UParticleModuleRotation.generated.h"
+
+UCLASS()
+class UParticleModuleRotation : public UParticleModule
+{
+	GENERATED_REFLECTION_BODY()
+public:
+	/**
+	 *	파티클의 초기 회전 (1 = 360 도)
+	 *	EmitterTime으로 값을 가져온다.
+	 */
+	UPROPERTY(EditAnywhere, Category="ParticleModuleRotation")
+	FRawDistributionFloat StartRotation;
+
+public:
+	UParticleModuleRotation();
+
+	virtual ~UParticleModuleRotation() = default;
+
+	void InitializeDefaults();
+
+	//~Begin UParticleModule 인터페이스
+	virtual void Spawn(const FSpawnContext& Context) override;
+	//~End UParticleModule 인터페이스
+};
